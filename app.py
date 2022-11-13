b5270a251549aa3a95ea7c163849bff5f7398ef3
barretto <b4rretto@gmail.com>
2022-11-12 21:14:13 -0500

    updating demo article and disclaimer

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ def getImage(prompt):
 
 demo = gr.Interface(
     fn = run,
+    article = "<p>For better accuracy, please enter the following information about the person you want to generate a portrait for.</p><p>Disclaimer: despite how impressive being able to turn text into image is, beware to the fact that this model may output content that reinforces or exacerbates societal biases, as well as realistic faces, pornography and violence. The model was trained on the LAION-5B dataset, which scraped non-curated image-text-pairs from the internet (the exception being the removal of illegal content) and is meant for research purposes.</p><p>You can read more in the model card <a href=\"https://huggingface.co/CompVis/stable-diffusion-v1-4\" target=\"_blank\">HERE.</a></p>"
+        ,
     inputs = [
+        
         gr.Radio(["Male", "Female"], label="Gender" ),  #Gender
         gr.Slider(100,250,value=160, label="Height (cm)"), #Height
         gr.Slider(40,250,value=50, label="Weight (kg)"), #Weight
@@ -32,7 +35,7 @@ demo = gr.Interface(
         gr.Dropdown(sorted(["South Asian", "North Asian", "White", "African American", "American Indian", "Hispanic"]), label="Ethnicity"), #Ethnicity
         gr.Dropdown(sorted(["Black", "Brown", "Yellow", "Peach","Tan","Beige", "White", "Grey"]), label="Skin color" ), #Skin color
         gr.Radio(sorted(["Short", "Long", "Bald", "Medium"]), label="Hair length" ), #Hair length
-        gr.Dropdown(sorted(["Black","Dark brown", "Light brown", "Blonde", "Red", "Grey"]), label="Hair color" ), #Hair color
+        gr.Dropdown(sorted(["Black","Dark brown", "Light brown", "Blond", "Red", "Grey"]), label="Hair color" ), #Hair color
         gr.Dropdown(sorted(["Black", "Dark Brown", "Light Brown", "Green", "Blue", "Hazel", "Amber", "Red", "Pink"]), label="Eye color" ), #Eye color
         gr.Radio(sorted(["Mustache", "Beard", "Unibrow"]), label="Facial hair" ), #Facial hair
         gr.Dropdown(sorted(["Ginger", "Black", "Brown", "Grey", "Yellow", "White"]), label="Facial hair color" ), #facial hair color
