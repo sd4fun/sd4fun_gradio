744b826f496bca5521f1162619bf4897724a1693
barretto <b4rretto@gmail.com>
2022-11-13 09:56:24 -0500

    updated the prompt to use BMI instead of height and weight

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ import requests
 url = "https://api.newnative.ai/stable-diffusion?prompt="
 
 def run(gender, height, weight, age, ethnicity, skin_color, hair_length, hair_color, eye_color, facial_hair, facial_hair_color):
-    prompt = f"""mugshot portrait, {age + " " if age else ""}{skin_color} {ethnicity} {gender}{" with " + hair_length if hair_length else ""} {hair_color + " hair, " if hair_length and hair_color else ""}{eye_color + " eyes, " if eye_color else ""}{facial_hair_color + " " if facial_hair and facial_hair_color else ""}{facial_hair+ ", " if facial_hair else ""}{getHeightInFeet(height) + " tall, " if height else ""}{getWeightInPounds(weight)}, canon EOS"""
+    bmi = weight/(height/100)**2
+
+    if bmi < 18.5:
+        bodyShape = "slim"
+    elif bmi < 25:
+        bodyShape = "average" 
+    elif bmi < 30:
+        bodyShape = "overweight"
+    else:
+        bodyShape = "obese"
+
+    prompt = f"""mugshot portrait, {age + " " if age else ""}{bodyShape} {skin_color} {ethnicity} {gender}{" with " + hair_length if hair_length else ""} {hair_color + " hair, " if hair_length and hair_color else ""}{eye_color + " eyes, " if eye_color else ""}{facial_hair_color + " " if facial_hair and facial_hair_color else ""}{facial_hair+ ", " if facial_hair else ""}canon EOS"""
     
-    print (prompt)
+    print (prompt.lower())
     return getImage(prompt.lower())
 
 def getHeightInFeet(height):
@@ -35,7 +46,7 @@ demo = gr.Interface(
         gr.Dropdown(sorted(["South Asian", "North Asian", "White", "African American", "American Indian", "Hispanic", "Latin"]), label="Race"), #Race
         gr.Dropdown(sorted(["Black", "Brown", "Yellow", "Peach","Tan","Beige", "White", "Grey"]), label="Skin color" ), #Skin color
         gr.Radio(sorted(["Short", "Long", "Bald", "Medium"]), label="Hair length" ), #Hair length
-        gr.Dropdown(sorted(["Black","Dark brown", "Light brown", "Blond", "Red", "Grey"]), label="Hair color" ), #Hair color
+        gr.Dropdown(sorted(["Black","Dark Brown", "Light Brown", "Blond", "Red", "Grey"]), label="Hair color" ), #Hair color
         gr.Dropdown(sorted(["Black", "Dark Brown", "Light Brown", "Green", "Blue", "Hazel", "Amber", "Red", "Pink"]), label="Eye color" ), #Eye color
         gr.Radio(sorted(["Mustache", "Beard", "Unibrow"]), label="Facial hair" ), #Facial hair
         gr.Dropdown(sorted(["Ginger", "Black", "Brown", "Grey", "Yellow", "White"]), label="Facial hair color" ), #facial hair color
@@ -45,12 +56,15 @@ demo = gr.Interface(
     outputs = "image",
     title = "AI Portrait Generator",
     description = "Generate a portrait of a person with the given attributes",
-    # examples=[
-    #     [2, "cat", "park", ["ran", "swam"], True],
-    #     [4, "dog", "zoo", ["ate", "swam"], False],
-    #     [10, "bird", "road", ["ran"], False],
-    #     [8, "cat", "zoo", ["ate"], True],
-    # ],
+    examples=[
+        ["Male", 180, 80, "Adult", "White", "White", "Short", "Blond", "Blue", "Mustache", "Ginger"],
+        ["Male", 160, 50, "Young", "Latin", "Brown", "Medium", "Dark Brown", "Dark Brown", "Beard", "Brown"],
+        ["Female", 150, 70, "Old", "South Asian", "White", "Long", "Black", "Light Brown", "Unibrow", "Brown"],
+        ["Female", 170, 60, "Middle-aged", "African american", "Black", "Medium", "Black", "Black", None, None],
+
+    
+
+    ],
 )
 
 if __name__ == "__main__":
